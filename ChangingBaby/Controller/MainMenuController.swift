363f9d172d7b4fd89c77dca2c787ed76094bc196
Yves Charpentier <charpentier.yves@live.fr>
2021-12-02 19:02:13 +0100

    minor changes

--- ChangingBaby/Controller/MainMenuController.swift
+++ ChangingBaby/Controller/MainMenuController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class MainMenuController: UIViewController {
+    @IBOutlet weak var interfaceView: UIView!
+    
+    @IBAction func showContactsView() {
+        interfaceView.backgroundColor = .magenta
+    }
+    @IBAction func showTips() {
+        interfaceView.backgroundColor = .blue
+    }
+    @IBAction func showMore() {
+        interfaceView.backgroundColor = .black
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
