ae0e3196dbb98a9d8ec75a5f23d050dfaa1562c7
YvesC88 <charpentier.yves@live.fr>
2023-07-31 13:00:35 +0200

    add new button for add place in mapView

--- ChangingBabyTests/FirebaseWrapperMock.swift
+++ ChangingBabyTests/FirebaseWrapperMock.swift
@@ -26,37 +26,37 @@ final class FirebaseWrapperMock: FirebaseProtocol {
     var isSignOutCalled: Bool = false
     var isForgetPwdCalled: Bool = false
     var isFetchPlaceCalled: Bool = false
-    
+
     func create(name: String, mail: String, password: String, completion: @escaping (String?, String?) -> ()) {
         isCreateCalled = true
         completion(createResult, createError)
     }
-    
+
     func addDocument(name: String, userId: String, completion: @escaping (String?) -> ()) {
         isAddDocumentCalled = true
         completion(createDbError)
     }
-    
+
     func updateUserProfile(userName: String, completion: @escaping (String?) -> ()) {
         isUpdateProfileCalled = true
         completion(createUpdateProfileError)
     }
-    
+
     func signIn(mail: String, password: String, completion: @escaping (String?, String?) -> ()) {
         isSignInCalled = true
         completion(signInResult, signInError)
     }
-    
+
     func signOut(completion: @escaping (String?, String?) -> ()) {
         isSignOutCalled = true
         completion(signOutResult, signOutError)
     }
-    
+
     func forgetPwd(mail: String, completion: @escaping (String?) -> ()) {
         isForgetPwdCalled = true
         completion(forgetPwdError)
     }
-    
+
     func fetch(collectionID: String, completion: @escaping ([Place]?, String?) -> ()) {
         isFetchPlaceCalled = true
         completion(placeResult, placeError)
